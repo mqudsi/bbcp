2c369dc4aadfd6797e1b2f37d0ac549167984c7a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-01-26 17:20:36 -0600

    Use meson to build project
    This is the initial attempt, not yet complete. Ultimately we'll get rid
    of all the platform defines and just test for features.

--- src/bbcp_Platform.h
+++ src/bbcp_Platform.h
@@ -4,7 +4,9 @@
 /*                                                                            */
 /*                       b b c p _ P l a t f o r m . h                        */
 /*                                                                            */
-/*(c) 2002-14 by the Board of Trustees of the Leland Stanford, Jr., University*//*      All Rights Reserved. See bbcp_Version.C for complete License Terms    *//*                            All Rights Reserved                             */
+/*(c) 2002-14 by the Board of Trustees of the Leland Stanford, Jr., University*/
+/*      All Rights Reserved. See bbcp_Version.C for complete License Terms    */
+/*                            All Rights Reserved                             */
 /*   Produced by Andrew Hanushevsky for Stanford University under contract    */
 /*              DE-AC02-76-SFO0515 with the Department of Energy              */
 /*                                                                            */
@@ -29,22 +31,8 @@
 
 #define PS_CMD "/bin/ps -o ppid -p %d"
 
-#ifdef AIX
-#define S_IAMB  0x1FF
-#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen,  rpnt, pretc) \
-     (rpnt = gethostbyname(hname))
-
-#define GETHOSTBYADDR(haddr,hlen,htype,rbuff,cbuff,cblen, rpnt,pretc) \
-     (rpnt = gethostbyaddr(haddr,hlen,htype))
-
-#define GETSERVBYNAME(name, stype, psrv, buff, blen,  rpnt) \
-     (rpnt = getservbyname(name, stype))
-
-#define EBADSLT EFAULT
-#endif
-
 #if defined(FREEBSD)
-#define SYS_ERR_DEF const char *const
+#define SYS_ERR_DEF const char* const
 #define SYS_NER_DEF const int
 #endif
 
@@ -54,25 +42,14 @@
 #endif
 #endif
 
-#if defined(FREEBSD) || defined(MACOS) || defined(AIX)
-
-#define S_IAMB  0x1FF
-
-#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen,  rpnt, pretc) \
-     (rpnt = gethostbyname(hname))
-
-#define GETHOSTBYADDR(haddr,hlen,htype,rbuff,cbuff,cblen, rpnt,pretc) \
-     (rpnt = gethostbyaddr(haddr,hlen,htype))
-
-#define GETSERVBYNAME(name, stype, psrv, buff, blen,  rpnt) \
-     (rpnt = getservbyname(name, stype))
-
+#ifndef S_IAMB
+#define S_IAMB 0x1FF
 #endif
 
 #ifdef LINUX
 
 // The value should be MAX_IOVEC but it's not defined in Linux, sigh
-//
+#define __need_IOV_MAX
 #ifndef IOV_MAX
 #define IOV_MAX 16
 #endif
@@ -83,31 +60,31 @@
 #define POLLRDBAND 0
 #endif
 
-#define SYS_ERR_DEF const char *const
-#define SYS_NER_DEF       int
+#define SYS_ERR_DEF const char* const
+#define SYS_NER_DEF int
 
-#define S_IAMB  0x1FF
+#define S_IAMB 0x1FF
 
 #define FMTLL "L"
 
 #ifdef CRAY_X1E
-#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen,  rpnt, pretc) \
-     (rpnt = gethostbyname(hname))
+#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen, rpnt, pretc) \
+    (rpnt = gethostbyname(hname))
 
-#define GETHOSTBYADDR(haddr,hlen,htype,rbuff,cbuff,cblen, rpnt,pretc) \
-     (rpnt = gethostbyaddr(haddr,hlen,htype))
+#define GETHOSTBYADDR(haddr, hlen, htype, rbuff, cbuff, cblen, rpnt, pretc) \
+    (rpnt = gethostbyaddr(haddr, hlen, htype))
 
-#define GETSERVBYNAME(name, stype, psrv, buff, blen,  rpnt) \
-     (rpnt = getservbyname(name, stype))
+#define GETSERVBYNAME(name, stype, psrv, buff, blen, rpnt) \
+    (rpnt = getservbyname(name, stype))
 #else
-#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen,  rpnt, pretc) \
-     (gethostbyname_r(hname, rbuff, cbuff, cblen, &rpnt, pretc) == 0 && rpnt)
+#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen, rpnt, pretc) \
+    (gethostbyname_r(hname, rbuff, cbuff, cblen, &rpnt, pretc) == 0 && rpnt)
 
-#define GETHOSTBYADDR(haddr,hlen,htype,rbuff,cbuff,cblen, rpnt,pretc) \
-     (gethostbyaddr_r(haddr,hlen,htype,rbuff,cbuff,cblen,&rpnt,pretc) == 0 && rpnt)
+#define GETHOSTBYADDR(haddr, hlen, htype, rbuff, cbuff, cblen, rpnt, pretc) \
+    (gethostbyaddr_r(haddr, hlen, htype, rbuff, cbuff, cblen, &rpnt, pretc) == 0 && rpnt)
 
-#define GETSERVBYNAME(name, stype, psrv, buff, blen,  rpnt) \
-     (getservbyname_r(name, stype, psrv, buff, blen, &rpnt) == 0 && rpnt)
+#define GETSERVBYNAME(name, stype, psrv, buff, blen, rpnt) \
+    (getservbyname_r(name, stype, psrv, buff, blen, &rpnt) == 0 && rpnt)
 #endif
 
 #else
@@ -116,29 +93,27 @@
 
 #endif
 
-#if !defined(LINUX) && !defined(FREEBSD) && !defined(AIX) && !defined(MACOS)
-
-#define SYS_ERR_DEF char *
-#define SYS_NER_DEF int
-
-#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen, rpnt, pretc) \
-(rpnt=gethostbyname_r(hname, rbuff, cbuff, cblen,       pretc))
-
-#define GETHOSTBYADDR(haddr, hlen, htype, rbuff, cbuff, cblen, rpnt, pretc) \
-(rpnt=gethostbyaddr_r(haddr, hlen, htype, rbuff, cbuff, cblen,       pretc))
-
-
-#define GETSERVBYNAME(name, stype, psrv, buff, blen, rpnt) \
-(rpnt=getservbyname_r(name, stype, psrv, buff, blen))
-
-#endif
-
-typedef       void * gsval_t;
-typedef       void * ssval_t;
+typedef void* gsval_t;
+typedef void* ssval_t;
 typedef size_t Size_T;
 
 #if defined(__FreeBSD__) || defined(MACOS) || defined(AIX)
 #define EBADSLT EFAULT
+#endif
+
+#ifndef GETHOSTBYNAME
+#define GETHOSTBYNAME(hname, rbuff, cbuff, cblen, rpnt, pretc) \
+    (rpnt = gethostbyname(hname))
+#endif
+
+#ifndef GETHOSTBYADDR
+#define GETHOSTBYADDR(haddr, hlen, htype, rbuff, cbuff, cblen, rpnt, pretc) \
+    (rpnt = gethostbyaddr(haddr, hlen, htype))
+#endif
+
+#ifndef GETSERVBYNAME
+#define GETSERVBYNAME(name, stype, psrv, buff, blen, rpnt) \
+    (rpnt = getservbyname(name, stype))
 #endif
 
 #endif
